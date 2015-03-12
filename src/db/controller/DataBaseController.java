baf24a5629c3ae008dc13101c2218f22963f8c9b
zaconder <zac@conder.us>
2015-03-12 13:02:24 -0600

    Added Documentation and Methods
    Made methods to convert 1d data into 2d data

--- src/db/controller/DataBaseController.java
+++ src/db/controller/DataBaseController.java
@@ -1,53 +1,52 @@
 package db.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.swing.JOptionPane;
+import java.sql.*;
+import javax.swing.*;
 
 /**
- * The controller that manages the connection to the database and what the connection will do.
+ * The controller that manages the connection to the database and what the
+ * connection will do.
+ * 
  * @author zcon5199
- * @version 1.3 3/4/2015
+ * @version 1.4 3/4/2015 Converted 1d data into 2d data.
  */
 public class DataBaseController
 {
 	private String connectionString;
 	private Connection databaseConnection;
 	private DataBaseAppController baseController;
-	
+
 	/**
-	 * This connects to the Database as user "root" and it accesses the database of "motorcycle"
+	 * This connects to the Database as user "root" and it accesses the database
+	 * of "motorcycle"
+	 * 
 	 * @param dataBaseAppController
 	 */
 	public DataBaseController(DataBaseAppController dataBaseAppController)
 	{
-		connectionString = "jdbc:mysql://127.0.0.1/motorcycle?user=root";
+		connectionString = "jdbc:mysql://localhost/motorcycle?user=root";
 		this.baseController = dataBaseAppController;
 		checkDriver();
 		setupConnection();
-		
+
 	}
-	
+
 	/**
-	 * This method will check the driver being used and if it's not the correct one, it will display an error.
+	 * This method will check the driver being used and if it's not the correct
+	 * one, it will display an error.
 	 */
 	private void checkDriver()
 	{
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-		}
-		catch(Exception currentException)
+		} catch (Exception currentException)
 		{
 			displayErrors(currentException);
 			System.exit(1);
 		}
 	}
-	
+
 	/**
 	 * Will close the Connection when the user closes the window.
 	 */
@@ -56,13 +55,12 @@ public class DataBaseController
 		try
 		{
 			databaseConnection.close();
-		}
-		catch (SQLException error)
+		} catch (SQLException error)
 		{
 			displayErrors(error);
 		}
 	}
-	
+
 	/**
 	 * This will grab the connection and from the database and establish it.
 	 */
@@ -71,59 +69,64 @@ public class DataBaseController
 		try
 		{
 			databaseConnection = DriverManager.getConnection(connectionString);
-		}
-		catch(SQLException currentException)
+		} catch (SQLException currentException)
 		{
 			displayErrors(currentException);
 		}
 	}
-	
+
 	/**
-	 * displayErrors is where the error codes are stored. It will grab the current exception and display it to the user.
-	 * @param currentException The error code.
+	 * displayErrors is where the error codes are stored. It will grab the
+	 * current exception and display it to the user.
+	 * 
+	 * @param currentException
+	 *            The error code.
 	 */
 	public void displayErrors(Exception currentException)
 	{
 		JOptionPane.showMessageDialog(baseController.getAppFrame(), currentException.getMessage());
-		
-		if(currentException instanceof SQLException)
+
+		if (currentException instanceof SQLException)
 		{
 			JOptionPane.showMessageDialog(baseController.getAppFrame(), "SQL State: " + ((SQLException) currentException));
 			JOptionPane.showMessageDialog(baseController.getAppFrame(), "SQL Error Code: " + ((SQLException) currentException));
 		}
 	}
-	
+
 	/**
-	 * Display tables will grab data (tables) from the Database and present it to the user inside the panel.
+	 * Display tables will grab data (tables) from the Database and present it
+	 * to the user inside the panel.
+	 * 
 	 * @return Will return the results of what the database has.
 	 */
 	public String displayTables()
 	{
 		String results = "";
 		String query = "SHOW TABLES";
-		
+
 		try
 		{
 			Statement firstStatement = databaseConnection.createStatement();
 			ResultSet answer = firstStatement.executeQuery(query);
-			while(answer.next())
+			while (answer.next())
 			{
 				results += answer.getString(1) + "\n";
 			}
 			answer.close();
 			firstStatement.close();
-		}
-		catch(SQLException currentSQLError)
+		} catch (SQLException currentSQLError)
 		{
 			displayErrors(currentSQLError);
 		}
-		
+
 		return results;
 	}
-	
+
 	/**
-	 * This will grab data from a specific table and describe the data inside of it.
-	 * @return Returns the data inside "glove" 
+	 * This will grab data from a specific table and describe the data inside of
+	 * it.
+	 * 
+	 * @return Returns the data inside "glove"
 	 */
 	public String describeTable()
 	{
@@ -133,19 +136,113 @@ public class DataBaseController
 		{
 			Statement firstStatement = databaseConnection.createStatement();
 			ResultSet answer = firstStatement.executeQuery(query);
+			while (answer.next())
+			{
+				results += answer.getString(1) + "\t" + answer.getString(2) + "\t" + answer.getString(3) + "\t" + answer.getString(4) + "\n";
+			}
+			answer.close();
+			firstStatement.close();
+		} catch (SQLException currentSQLError)
+		{
+			displayErrors(currentSQLError);
+		}
+
+		return results;
+
+	}
+
+	/**
+	 * This is where the SQL code gets used and actually inserts data into the DataBase.
+	 * @return Will return how many rows have been affected by this.
+	 */
+	public int insertSample()
+	{
+		int rowsAffected = 0;
+		String insertQuery = "INSERT INTO `motorcycle`.`motorcycles`"
+		+ " (`id`, `color`, `type_motorcycle`, `top_speed`, `engine_size_cc`, `manufacturer`, `year_made`, `name`)"
+		+ " VALUES (NULL, 'Green', 'Sport', '165', '300', 'Kawasaki', '2014', 'Kawasaki Ninja');";
+
+		try
+		{
+			Statement insertStatement = databaseConnection.createStatement();
+			rowsAffected = insertStatement.executeUpdate(insertQuery);
+			insertStatement.close();
+		} catch (SQLException currentSQLError)
+		{
+			displayErrors(currentSQLError);
+		}
+
+		return rowsAffected;
+	}
+
+	
+	/**
+	 * Will convert 1d data into 2d data
+	 * @return will display the information that has been converted
+	 */
+	public String[][] tableInfo()
+	{
+		
+	String[][] results;
+	String query = "SHOW TABLES";
+	
+		try
+		{
+			Statement firstStatement = databaseConnection.createStatement();
+			ResultSet answer = firstStatement.executeQuery(query);
+			int rowCount;
+			answer.last();
+			rowCount = answer.getRow();
+			answer.beforeFirst();
+			results = new String [rowCount][1];
+			
 			while(answer.next())
 			{
-				results += answer.getString(1) + "\t" + answer.getString(2) + "\t" + answer.getString(3) + "\t" + answer.getString(4) + "\n" ;
+				results[answer.getRow() - 1][0] = answer.getString(1);
 			}
+			
 			answer.close();
 			firstStatement.close();
 		}
 		catch(SQLException currentSQLError)
 		{
+			results = new String [][] {{"you get, NOTHING!!!! :D"}};
 			displayErrors(currentSQLError);
 		}
 		
 		return results;
-		
 	}
+
+	/**
+	 * Will get the metaData that is in the query
+	 * @return returns the column info of the table
+	 */
+	public String[] getMetaData()
+	{
+		String[] colInfo;
+		String query = "SHOW TABLES";
+
+		try
+		{
+			Statement firstStatement = databaseConnection.createStatement();
+			ResultSet answer = firstStatement.executeQuery(query);
+			ResultSetMetaData myMeta = answer.getMetaData();
+
+			colInfo = new String[myMeta.getColumnCount()];
+			for (int spot = 0; spot < myMeta.getColumnCount(); spot++)
+			{
+				colInfo[spot] = myMeta.getColumnName(spot + 1);
+			}
+
+			answer.close();
+			firstStatement.close();
+		} catch (SQLException currentSQLError)
+		{
+			colInfo = new String[] { "NOTHING!!" };
+			displayErrors(currentSQLError);
+		}
+
+		return colInfo;
+	}
+
 }
--- src/db/view/DataBasePanel.java
+++ src/db/view/DataBasePanel.java
@@ -5,45 +5,65 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.SpringLayout;
+import javax.swing.table.DefaultTableModel;
 
 import db.controller.DataBaseAppController;
 
 public class DataBasePanel extends JPanel
 {
+	
+	Color aColor = new Color(669900);
+	Color bColor = new Color(0xFF9900);
+	
 	private DataBaseAppController baseController;
 	
 	private JButton appButton;
 	private JButton appButton2;
+	private JButton appButton3;
 	private JTextArea displayArea;
 	private JScrollPane displayPane;
 	private SpringLayout baseLayout;
+	private JTable tableData;
 	
 	public DataBasePanel(DataBaseAppController baseController)
 	{
 		this.baseController = baseController;
+		baseLayout = new SpringLayout();
 		appButton = new JButton("Describe the Query");
 		appButton2 = new JButton("Test the query");
+		appButton3 = new JButton("Insert into the Database");
+
 		displayArea = new JTextArea(10, 30);
 		displayPane = new JScrollPane(displayArea);
-		baseLayout = new SpringLayout();
-		
-		
+			
+		setupTable();
 		setupPane();
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
 	
+	private void setupTable()
+	{
+		tableData = new JTable(new DefaultTableModel(baseController.getDataBase().tableInfo(), baseController.getDataBase().getMetaData()));
+		displayPane = new JScrollPane(tableData);
+	}
+	
+	
 	private void setupPanel()
 	{
-		this.setBackground(Color.MAGENTA);
+		this.setBackground(bColor);
+		this.setSize(1000,1000);
 		this.setLayout(baseLayout);
 		this.add(appButton);
 		this.add(appButton2);
+		this.add(appButton3);
 		this.add(displayPane);
 	}
 	
@@ -67,6 +87,16 @@ public class DataBasePanel extends JPanel
 			}
 		});
 		
+		appButton3.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				int answer = baseController.getDataBase().insertSample();
+//				displayArea.setText(displayArea.getText() + "\nRows Affected: " + answer);
+				JOptionPane.showMessageDialog(displayArea, "\nRows Affected: " + answer);
+			}
+		});
+		
 	}
 		
 		
@@ -75,10 +105,11 @@ public class DataBasePanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.NORTH, displayPane, 40, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, displayPane, 20, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, appButton2, 160, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, appButton3, 290, SpringLayout.WEST, this);
 	}
 	
 	private void setupPane()
 	{
-
+		
 	}
 }
